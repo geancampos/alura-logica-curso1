51f37a1ef7c1061f693fd2413f6d6508f04ef25d
geancampos <gean699@hotmail.com>
2024-05-26 21:48:38 -0300

    curso 1 de lógica concluído

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 alert("Boas vindas ao jogo do número secreto");
-let numeroSecreto = 29;
-prompt("Escolha um número entre 1 e 30");
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1)
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+  chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+  if (chute == numeroSecreto) {
+  } else {
+    if (chute > numeroSecreto) {
+      alert(`O número secreto é menor que ${chute}`);
+    } else {
+      alert(`O número secreto é maior que ${chute}`);
+    }
+    tentativas++;
+  }
+}
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Isso aí! Você descobriu o número secreto: ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+// if (tentativas > 1) {
+//     alert(`Isso aí! Você descobriu o número secreto: ${numeroSecreto} com ${tentativas} tentativas`);
+// } else {
+//     alert(`Isso aí! Você descobriu o número secreto: ${numeroSecreto} com ${tentativas} tentativa`);
+// }
